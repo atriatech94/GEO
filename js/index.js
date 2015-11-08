d4e750bbe4f6646eb0383711e24ab48191dc81c9
atriatech94 <hamid_7555@hotmail.com>
2015-11-08 11:49:11 +0330

    n4

--- js/index.js
+++ js/index.js
@@ -67,32 +67,27 @@ function amintest(){
            
     // device APIs are available
     //
-var myService;
+  var myService;
 
-document.addEventListener('deviceready', function() {
-   myService = cordova.plugins.myService;
-   go();
-}, true);
+   document.addEventListener('deviceready', function() {
+      var serviceName = 'com.red_folder.phonegap.plugin.backgroundservice.sample.MyService';
+      var factory = cordova.require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService')
+      myService = factory.create(serviceName);
 
-function go() {
-   myService.getStatus(function(r){startService(r)}, function(e){handleError(e)});
-};
+      getStatus();
+   }, true);
 
-function startService(data) {
-   if (data.ServiceRunning) {
-      enableTimer(data);
-   } else {
-      myService.startService(function(r){enableTimer(r)}, function(e){handleError(e)});
+   function getStatus() {
+      myService.getStatus(function(r){displayResult(r)}, function(e){displayError(e)});
    }
-}
 
-function enableTimer(data) {
-   if (data.TimerEnabled) {
-      allDone();
-   } else {
-      myService.enableTimer(60000, function(r){allDone(r)}, function(e){handleError(e)});
+   function displayResult(data) {
+      alert("Is service running: " + data.ServiceRunning);
    }
-}
+
+   function displayError(data) {
+      alert("We have an error");
+   }
 
 function allDone() {
    alert("Service now running");
