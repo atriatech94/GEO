3961acd6dc48384916b711705da1e3de591350f3
atriatech94 <hamid_7555@hotmail.com>
2015-12-08 18:31:38 +0330

    n33

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
-var gpss = 0;
-bb= 0;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
 document.addEventListener("backbutton",amintest, false);
 function amintest(){
     
@@ -56,69 +74,56 @@ function amintest(){
 
     // Called when background mode has been activated
     cordova.plugins.backgroundMode.onactivate = function () {
-        setInterval(function(){geoFindMe()},50000); 
-        
+        setInterval(function(){geoFindMe();},50000);
     }
 }, false);
+    //
+            function geoFindMe() {
+               
+                if(localStorage.getItem("user_pass")!=null)
+                {
+                    var output = document.getElementById("out");
+                    if (!navigator.geolocation){
+                        return;
+                    }
+                    function success(position) {
 
-setInterval(function(){   navigator.geolocation.getCurrentPosition(onSuccessw,onErrorw,{timeout:10000});},1000)
-function onSuccessw(){gpss =  1;console.log("gps is on");}
-function onErrorw(){gpss =  0;console.log("gps is off");}
-setTimeout(function(){geoFindMe()},5000); 
-function geoFindMe() {
-        
-    if(localStorage.getItem("user_pass")!=null)
-    {
-                   
-        navigator.geolocation.getCurrentPosition(GetLocation);
-                     
-        function GetLocation(location) {
-            
-            if(gpss==0){return false;}
-            
-            lat = location.coords.latitude;
-            lon = location.coords.longitude;
-           
-            now_node.push({'mn_lat':lat ,'mn_lon': lon,'mn_date':  js_yyyy_mm_dd_hh_mm_ss() ,"marketer_id" : localStorage.getItem("user_id") });
-            jnow_node = JSON.stringify(now_node);
-            localStorage.setItem("now_node",jnow_node);
-            bb++;
-            if(bb > 5)
-            {
-                $.post(base_url+'/api/marketer_now/nima564321/',{ponits : localStorage.getItem("now_node") },function(){
-                    localStorage.removeItem("now_node");
-                    localStorage.setItem("now_node",null);
-                    bb = 0;
-                    now_node = [] ;
-                     console.log("send");
-
-                }).fail(function(){
-                    console.log("Fail");            
-                });
-            }
+                        now_node.push({'mn_lat':position.coords.latitude ,'mn_lon':position.coords.longitude,'mn_date':  js_yyyy_mm_dd_hh_mm_ss() ,"marketer_id" : localStorage.getItem("user_id") });
+                        jnow_node = JSON.stringify(now_node);
+                        localStorage.setItem("now_node",jnow_node);
+                        bb++;
+                        if(bb > 5)
+                        {
+                           $.post('http://www.simanfars.ir/marketer/api/marketer_now/nima564321/',{ponits : localStorage.getItem("now_node") },function(){
+                               localStorage.removeItem("now_node");
+                               bb = 0;
+                               now_node = [] ;
+                               
+                           }).fail(function(){
+                               console.log("Fail");            
+                           });
+                        }
+                       
                         
-        }/*GetLocation*/
-       
-                   
-    }/*end localstroge*/
-    setTimeout(function(){geoFindMe()},5000); 
-}/*end function*/
+                    };
+                    function error() {};
+                    navigator.geolocation.getCurrentPosition(success, error);
+                }/*end localstroge*/
+                
+            }/*end function*/
+            
+            function js_yyyy_mm_dd_hh_mm_ss () 
+            {
+              now = new Date();
+              year = "" + now.getFullYear();
+              month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+              day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+              hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+              minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+              second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+              return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+            }
 
-/*==================================================================================*/
-
-function js_yyyy_mm_dd_hh_mm_ss () 
-{
-    now = new Date();
-    year = "" + now.getFullYear();
-    month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
-    day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
-    hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
-    minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
-    second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
-    return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
-}
-/*==================================================================================*/
-/*==================================================================================*/
 $(function(){
     $(window).resize(function(){
        $('body,html').height( $(window).height());
